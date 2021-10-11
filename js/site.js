d871df9cea410aa9355a4bff87ea0f2e33eaef19
a-santiago <santiago.62596@gmail.com>
2021-10-11 12:54:24 -0400

    commit_001
    Application has been completed

--- js/site.js
+++ js/site.js
@@ -1,29 +1,50 @@
+document.getElementById("btnSubmit").addEventListener("click", Answer);
 
-//Displays a message to the User
-function getString() {
-    let userString = document.getElementById("userstring").value;
+function sumOfAllFears(userArray, numk) {
 
-    let revString = reverseString(userString)
 
-    displayString(revString);
+    let found = false;
+
+    for (let i = 0; i <= userArray.length - 2; i++) {
+
+        for (let y = (i + 1); y <= userArray.length - 1; y++) {
+            if (numk == userArray[i] + userArray[y]) {
+                found = true;
+                return found;
+
+            }
+        }
+    }
+
+
+    return found;
 
 }
 
-function reverseString(userString) {
-    let startValue = userString.length - 1;
-    let endValue = 0;
+function Answer() {
+    let numk = parseInt(document.getElementById("inputk").value);
 
-    let revString = "";
 
-    for (let index = startValue; index <= endValue; index--){
-        revString = + userString[index];
-    };
-    return revString
-    
+
+    let num1 = parseInt(document.getElementById("input1").value);
+    let num2 = parseInt(document.getElementById("input2").value);
+    let num3 = parseInt(document.getElementById("input3").value);
+    let num4 = parseInt(document.getElementById("input4").value);
+
+    let userArray = [];
+
+    let output1 = document.getElementById("results");
+    let output2 = document.getElementById("results2");
+
+    userArray.push(num1, num2, num3, num4);
+
+    output1.innerHTML = userArray;
+    let found = sumOfAllFears(userArray, numk);
+    output2.innerHTML = found;
+
 }
+document.getElementById("btnClear").addEventListener("click", function () {
+    document.getElementById("results").innerText = " ";
+    document.getElementById("results2").innerText = " ";
 
-function displayString(revString) {
-    let output = document.getElementById("results");
-
-    output.innerHTML = revString;
-}
+});
